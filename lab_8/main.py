84721d93ae0ae7695d70886bd4bbbe5aaea84f7b
Ping2win <40270221+PINGWINbl4@users.noreply.github.com>
2021-11-24 17:30:09 +0500

    Add files via upload

--- lab_8/main.py
+++ lab_8/main.py
@@ -1,42 +1,37 @@
+print('значения координат находятся в диапозоне от 1 до 8')
 # ввод всех переменных
 while True:
     try:
+        x1 = int(input('Введите х1 '))
+        y1 = int(input('Введите y1 '))
+        x2 = int(input('Введите х2 '))
+        y2 = int(input('Введите y2 '))
+        if not(x1<9 and x2<9 and y1<9 and y2<9 and x1>0 and x2>0 and y1>0 and y2>0):
+            print('невозможная координата')
+            continue
         figure = int(input('Введите номер фигуры\n(1-ладья    2-слон      3-ферзь   4-конь) '))
         if figure>4:
             print('фигуры под таким номером нет')
         elif figure == 4:
             print("Для фигуры конь не доступны некоторрые функции программы ")
-        x1 = int(input('Введите х1 '))
-        if x1 in range(8):
-            y1 = int(input('Введите y1 '))
-            if y1 in range(8):
-                x2 = int(input('Введите х2 '))
-                if x2 in range(8):
-                    y2 = int(input('Введите y2 '))
-                    if y2 not in range(8):
-                        print('число должно быть <9')
-                else:
-                    print('число должно быть <9')
-            else:
-                print('число должно быть <9')
-        else:
-            print('число должно быть <9')
         break
     except(Exception):
         print('Ввод некорректен')
 
-# проверка цвета
+# цвет клеток
 if (x1+y1) % 2 == (x2+y2) % 2:
     print('цвет клеток совпадает')
 else:
     print('цвет клеток не совпадает')
-# проверка ладьи
+
+# проверка фигур
+# ладья
 if figure == 1:
     if x1 == x2 or y1 == y2:
         print('фигура угражает клетке')
     else:
         print('фигура не угражает клетке\n Возникнет угроза при ходе', x2, y1)
-# проверка слона
+# слон
 if figure == 2:
     if abs(x1-x2) == abs(y1-y2):
         print('фигура угражает клетке')
@@ -49,14 +44,14 @@ if figure == 2:
                 if abs(x1+i-x2) == abs(y1+i-y2):
                     print('Возникнет угроза при ходе', x1+i,y1+i)
                 if abs(x1-i-x2) == abs(y1-i-y2):
-                    print('Возникнет угроза при ходе', x1-i,y1-i)
-# проверка ферьзя
+                    print('Возникнет угроза при ходе', x1+i,y1+i)
+# ферзь
 if figure == 3:
     if abs(x1-x2) == abs(y1-y2) or x1 == x2 or y1 == y2:
         print('фигура угражает клетке')
     else:
         print('фигура не угражает клетке\n Возникнет угроза при ходе', x2, y1)
-# проверка коня
+# конь
 if figure == 4:
     if (x1+2 == x2 and y1+1 == y2) or (x1+1 == x2 and y1+2 == y2) or (x1-1 == x2 and y1+2 == y2) or (x1-2 == x2 and y1+1 == y2) or (x1-2 == x2 and y1-1 == y2) or (x1-1 == x2 and y1-2 == y2) or (x1+1 == x2 and y1-2 == y2) or (x1+2 == x2 and y1-1 == y2):
         print('фигура угражает клетке')
